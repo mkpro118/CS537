93e0694bf92f0d80b544c75c98b9cf0fd7746d5f
M K <mkpro118@gmail.com>
2023-10-23 23:05:52 -0500

    randomize

--- P4/plot_graph.py
+++ P4/plot_graph.py
@@ -6,16 +6,17 @@ data = np.genfromtxt('data.csv', delimiter=',')
 pids, proc_ticks, global_ticks, = data[:, 0], data[:, 1], data[:, 2]
 
 unique_pids = np.unique(pids)
+np.random.shuffle(unique_pids)
 
 fig, ax = plt.subplots(nrows=1, ncols=1)
 
 for pid in unique_pids:
     idxs = (pids == pid)
 
-    ax.plot(global_ticks[idxs], proc_ticks[idxs], label=f'pid = {pid}')
+    ax.plot(global_ticks[idxs], proc_ticks[idxs], label=f'{pid = }')
 
-ax.xlabel('Global Ticks')
-ax.ylabel('Process Ticks')
+ax.set_xlabel('Global Ticks')
+ax.set_ylabel('Process Ticks')
 
 fig.suptitle('Ticks per process vs Time')
 fig.legend()
