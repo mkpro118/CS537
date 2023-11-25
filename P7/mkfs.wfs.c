f687813e5a4b0ee38014bc8f2c35640a110ef592
M K <mkpro118@gmail.com>
2023-11-25 15:13:11 -0600

    Setup (I have no idea what I'm doing)

---
+++ P7/mkfs.wfs.c
@@ -0,0 +1,11 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+#include "wfs.h"
+
+
+int main(int argc, char const *argv[]) {
+    return 0;
+}
---
+++ P7/mount.wfs.c
@@ -0,0 +1,48 @@
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+static int wfs_getattr(const char* path, struct stat* stbuf) {
+    return 0;
+}
+
+static int wfs_mknod(const char* path, mode_t mode, dev_t rdev) {
+    return 0;
+}
+
+static int wfs_mkdir(const char* path, mode_t mode) {
+    return 0;
+}
+
+static int wfs_read(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
+    return 0;
+}
+
+static int wfs_write(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi) {
+    return 0;
+}
+
+static int wfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
+    return 0;
+}
+
+static int wfs_unlink(const char* path) {
+    return 0;
+}
+
+
+static struct fuse_operations ops = {
+    .getattr = wfs_getattr,
+    .mknod   = wfs_mknod,
+    .mkdir   = wfs_mkdir,
+    .read    = wfs_read,
+    .write   = wfs_write,
+    .readdir = wfs_readdir,
+    .unlink  = wfs_unlink,
+};
+
+
+int main(int argc, char *argv[]) {
+    return fuse_main(argc, argv, &ops, NULL);
+}
