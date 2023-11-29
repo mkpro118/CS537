c660a5e0172902c9e998d9c7a64373398410b205
M K <mkpro118@gmail.com>
2023-11-28 20:12:03 -0600

    idek what I did

--- P7/mount.wfs.c
+++ P7/mount.wfs.c
@@ -6,25 +6,44 @@
 #include <sys/stat.h>
 #include <unistd.h>
 
-#define FUSE_USE_VERSION 30
-#include <fuse.h>
+// #define FUSE_USE_VERSION 30
+// #include <fuse.h>
+
+#ifndef FUSE_USE_VERSION
+struct fuse_file_info {};
+enum fuse_readdir_flags {
+        FUSE_READDIR_PLUS = (1 << 0)
+};
+
+enum fuse_fill_dir_flags {
+        FUSE_FILL_DIR_PLUS = (1 << 1)
+};
+typedef int(* fuse_fill_dir_t) (void *buf, const char *name, const struct stat *stbuf, off_t off, enum fuse_fill_dir_flags flags);
+struct fuse_operations {
+    int (*getattr)(const char* path, struct stat* stbuf);
+    int (*mknod)(const char* path, mode_t mode, dev_t rdev);
+    int (*mkdir)(const char* path, mode_t mode);
+    int (*read)(const char* path, char *buf, size_t size, off_t offset, struct fuse_file_info* fi);
+    int (*write)(const char* path, const char *buf, size_t size, off_t offset, struct fuse_file_info* fi);
+    int (*readdir)(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi);
+};
+#endif
 
 
-#define ITOPSC  0 /* ITable operation succeeded */
-#define EITWNB -1 /* Error ITable Was Not Built */
-#define EITWR  -2 /*   Error ITable Was Reset   */
-#define EITWNR -3 /* Error ITable Was Not Reset */
+#define ITOPSC  0 /* I-Table operation succeeded */
+#define EITWNB -1 /* Error I-Table Was Not Built */
+#define EITWR  -2 /*   Error I-Table Was Reset   */
+#define EITWNR -3 /* Error I-Table Was Not Reset */
 
 static FILE* img_file = NULL;
 
 /* Something like a cache for faster lookups from the disk image */
 static struct {
     unsigned int* table;
-    unsigned int  size;
     unsigned int  capacity;
 } itable = {
-    .table = NULL,
-    .size = 0,
+    .table    = NULL,
+    .capacity = 0,
 };
 
 static const unsigned int ITABLE_CAPACITY_INCREMENT = 10;
@@ -38,7 +57,7 @@ static const unsigned int ITABLE_CAPACITY_INCREMENT = 10;
  * @param inode_number [description]
  * @param offset       [description]
  */
-static void fll_i_table(FILE* img_file, unsigned int inode_number, off_t offset) {
+static void fill_itable(FILE* img_file, unsigned int inode_number, off_t offset) {
     // If inode table is not yet present, we [re]started the FS
     // We detect whether or not inode table is present using the size
     // value in itable. If size = 0, then we have no information yet
@@ -46,7 +65,12 @@ static void fll_i_table(FILE* img_file, unsigned int inode_number, off_t offset)
     // minimum value of size would be 1 for the FS.
 }
 
-static int icrease_i_table_capacity(unsigned int capacity) {
+/**
+ * Increase the capacity of the I-Table
+ * @param  capacity [description]
+ * @return          [description]
+ */
+static int icrease_itable_capacity(unsigned int capacity) {
     int* temp;
     switch (itable.capacity) {
     case 0:
@@ -60,7 +84,6 @@ static int icrease_i_table_capacity(unsigned int capacity) {
             perror("FATAL ERROR: Failed to increase capacity (Malloc failed)!\n");
             perror("ABORTING Increase itable Capacity operation! Reset itable capacity and size to 0.");
 
-            itable.size = 0;
             itable.capacity = 0;
 
             perror("Future operations should try to re-read the disk image to re-build the itable.\n");
@@ -89,8 +112,10 @@ static int icrease_i_table_capacity(unsigned int capacity) {
     return ITOPSC;
 }
 
-static int read_image_file(file) {
+static int read_image_file(FILE* file) {
+    fseek(file, 0, SEEK_SET);
 
+    return 0;
 }
 
 static int wfs_getattr(const char* path, struct stat* stbuf) {
@@ -114,8 +139,8 @@ static int wfs_write(const char* path, const char *buf, size_t size, off_t offse
 }
 
 static int wfs_readdir(const char* path, void* buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info* fi) {
-    filler(buf,  ".", NULL, 0); // Current  directory
-    filler(buf, "..", NULL, 0); // Previous directory
+    filler(buf,  ".", NULL, 0, 0); // Current  directory
+    filler(buf, "..", NULL, 0, 0); // Previous directory
     return 0;
 }
 
@@ -140,13 +165,13 @@ int main(int argc, const char *argv[]) {
         return 0;
     }
 
+    img_file = fopen(argv[argc - 2], "ab+");
+
+    int fuse_argc = argc - 1;
+
     img_file = argv[argc - 2];
-    argv[argc - 2] = argv[--argc];
-    argv[argc] = NULL;
-    for (int i = 0; i < argc; i++) {
-        printf("%s ", argv[i]);
-    }
-    printf("\n");
-    return 0;
-    // return fuse_main(argc, argv, &ops, NULL);
+    argv[argc - 2] = argv[fuse_argc];
+    argv[fuse_argc] = NULL;
+
+    return fuse_main(fuse_argc, argv, &ops, NULL);
 }
