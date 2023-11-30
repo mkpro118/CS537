1aedb49e5ae2d01873fbcf2468c4802401f59ca7
SAANVI MALHOTRA <malhotra@rockhopper-01.cs.wisc.edu>
2023-11-29 21:10:48 -0600

    idoneagainhavefun

--- P7/mount.wfs.c
+++ P7/mount.wfs.c
@@ -351,12 +351,16 @@ int main(int argc, char *argv[]) {
         printf("Usage: $ mount.wfs [FUSE options] disk_path mount_point\n");
         return 0;
     }
-
+	ps_sb.disk_filename = strdup(argv[argc - 2]);
+	if(!ps_sb.disk_filename){
+        WFS_ERROR("strdup failed :(");
+        exit(ITOPFL);
+	}
     WFS_INFO("disk_path = %s\n", argv[argc - 2]);
-    ps_sb.disk_file = fopen(argv[argc - 2], "a+");
+    ps_sb.disk_file = fopen(ps_sb.disk_filename, "a+");
 
     if (!ps_sb.disk_file) {
-        WFS_ERROR("Couldn't open file \"%s\"\n", argv[argc - 2]);
+        WFS_ERROR("Couldn't open file \"%s\"\n", ps_sb.disk_filename);
         exit(ITOPFL);
     }
 
